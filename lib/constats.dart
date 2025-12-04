5ac9dfaa49c186134add4481ab76c99eb989ea74
abdelrahman1803 <abdozizo1115@gmail.com>
2025-12-04 15:19:13 +0200

    update styles

--- lib/constats.dart
+++ lib/constats.dart
@@ -1,2 +1,3 @@
 const kPrimaryColor = 0xff100B20;
 const kTransitionDuration = Duration(milliseconds: 250);
+const kGtSectraFine = "GT Sectra Fine";
--- lib/features/home/UI/views/widgets/home_view_body.dart
+++ lib/features/home/UI/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utilities/assets.dart';
 import 'package:bookly_app/core/utilities/styles.dart';
 import 'package:bookly_app/features/home/UI/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/features/home/UI/views/widgets/featured_list_view.dart';
@@ -16,9 +17,60 @@ class HomeViewBody extends StatelessWidget {
           CustomAppBar(),
           FeaturedBooksListView(),
           SizedBox(height: 36),
-          Text("Best Seller", style: Styles.titleBold),
+          Text("Best Seller", style: Styles.textStyle20),
+          BestSellerListViewItem(),
         ],
       ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          height: 100,
+          child: AspectRatio(
+            aspectRatio: 0.7,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage(AssetsData.testImage)),
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+        ),
+        SizedBox(width: 20),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Book Name", style: Styles.textStyle18, maxLines: 2),
+            Text("Writer Name", style: Styles.textStyle16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("19.99 ", style: Styles.textStyle20),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.star,
+                      color: const Color.fromARGB(255, 225, 193, 76),
+                      size: 24,
+                    ),
+                    Text("4.8", style: Styles.textStyle16),
+                    Text("(2280)", style: Styles.textStyle16),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
