983714cf75029e758a3f18887e1347ee1fb59078
abdelrahman1803 <abdozizo1115@gmail.com>
2025-12-07 14:09:42 +0200

    refactor code

--- lib/features/search/UI/views/widgets/search_view_body.dart
+++ lib/features/search/UI/views/widgets/search_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:bookly_app/core/utilities/styles.dart';
-import 'package:bookly_app/features/home/UI/views/widgets/book_list_view_item.dart';
 import 'package:bookly_app/features/search/UI/views/widgets/custom_search_text_field.dart';
+import 'package:bookly_app/features/search/UI/views/widgets/search_result_list_view.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -25,24 +25,6 @@ class SearchViewBody extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class SearchResultListView extends StatelessWidget {
-  const SearchResultListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 8.0),
-          child: BookListViewItem(),
-        );
-      },
     );
   }
 }
