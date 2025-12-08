93d0928b015c672b0657984b88d25ed10b7ef41c
abdelrahman1803 <abdozizo1115@gmail.com>
2025-12-08 18:00:19 +0200

    modify name

--- lib/features/home/data/apis/api_services.dart
+++ lib/features/home/data/apis/api_services.dart
@@ -1,11 +1,11 @@
 import 'package:dio/dio.dart';
 
-class HomeApiServices {
+class ApiServices {
   final String _baseUrl = "https://www.googleapis.com/books/v1/";
   final String _endPoint = "volumes?q=programming";
   final Dio dio;
 
-  HomeApiServices({required this.dio});
+  ApiServices({required this.dio});
 
   Future<Map<String, dynamic>> get({required String url}) async {
     Response response = await dio.get("$_baseUrl$_endPoint");
