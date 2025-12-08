ad98c411c3bf660446912e2ed5a3637e1ffb7315
abdelrahman1803 <abdozizo1115@gmail.com>
2025-12-08 18:25:47 +0200

    implement fetch newest books

--- lib/features/home/data/apis/api_services.dart
+++ lib/features/home/data/apis/api_services.dart
@@ -2,13 +2,12 @@ import 'package:dio/dio.dart';
 
 class ApiServices {
   final String _baseUrl = "https://www.googleapis.com/books/v1/";
-  final String _endPoint = "volumes?q=programming";
   final Dio dio;
 
   ApiServices({required this.dio});
 
-  Future<Map<String, dynamic>> get({required String url}) async {
-    Response response = await dio.get("$_baseUrl$_endPoint");
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    Response response = await dio.get("$_baseUrl$endPoint");
     return response.data;
   }
 }
--- lib/features/home/data/repo/home_repo_implementaion.dart
+++ lib/features/home/data/repo/home_repo_implementaion.dart
@@ -1,13 +1,28 @@
 import 'package:bookly_app/core/errors/error_handler.dart';
+import 'package:bookly_app/features/home/data/apis/api_services.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
-class HomeRepoImplementation implements HomeRepo{
+class HomeRepoImplementation implements HomeRepo {
+  final ApiServices apiServices;
+
+  HomeRepoImplementation({required this.apiServices});
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiServices.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming',
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
