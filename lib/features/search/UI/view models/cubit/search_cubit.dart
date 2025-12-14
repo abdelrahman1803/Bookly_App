02c33071b57d239712887939218c9854115a1a37
abdelrahman1803 <abdelrahman.cis.eng@gmail.com>
2025-12-14 19:25:34 +0200

    create search books cubit

---
+++ lib/features/search/UI/view models/cubit/search_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/search/data/repo/search_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'search_state.dart';
+
+class SearchCubit extends Cubit<SearchState> {
+  SearchCubit(this.searchRepo) : super(SearchInitial());
+  final SearchRepo searchRepo;
+  Future<void> fetchSearchBooks({required String title}) async {
+    emit(SearchLoading());
+    var result = await searchRepo.fetchSearchBooks(title: title);
+    result.fold(
+      (failure) {
+        emit(SearchFailure(failure.errMessage));
+      },
+      (books) {
+        emit(SearchSuccess(books));
+      },
+    );
+  }
+}
